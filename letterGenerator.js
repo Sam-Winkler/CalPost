6a0bce79757389faa4960dd200f3044cf89eb89b
Sam Winkler <swink85@gmail.com>
2025-03-09 20:39:51 -0700

    update api call

--- letterGenerator.js
+++ letterGenerator.js
@@ -98,7 +98,7 @@ function getEmailFromName(name, isSenateMember) {
 async function findLegislators(address, city, state, zip) {
     try {
         const fullAddress = `${address}, ${city}, ${state} ${zip}`;
-        const apiKey = window.CONFIG.GOOGLE_API_KEY;
+        const apiKey = 'AIzaSyBu2X2U1n96YcTknA8LFva3PMqWtGZJXuA';
 
         const url = `https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=${encodeURIComponent(fullAddress)}&levels=administrativeArea1&key=${apiKey}`;
 
